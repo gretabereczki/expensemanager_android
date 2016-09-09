c10d9ac88bf748161cbf071cdafe5d3c696bd4de
Greta Bereczki <greta.bereczki@gmail.com>
2016-09-09 10:58:48 +0200

    feat: Added possibility to set initial balance.

--- app/src/main/java/gretab/expensemanager/Settings.java
+++ app/src/main/java/gretab/expensemanager/Settings.java
@@ -1,6 +1,7 @@
 package gretab.expensemanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * The activity managing the settings.
@@ -18,6 +20,7 @@ public class Settings extends Activity {
 
     DatabaseHelper db;
     Spinner dropDown;
+    EditText balance;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +28,15 @@ public class Settings extends Activity {
         db = new DatabaseHelper(this);
 
         dropDown = (Spinner) findViewById(R.id.spinner2);
+        balance= (EditText) findViewById(R.id.editText3);
         String[] currency = new String[]{"GBP (£)", "USD ($)", "EUR (€)", "HUF"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, currency);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropDown.setAdapter(adapter);
+
+        SharedPreferences preferences=getSharedPreferences("Preferences", MODE_PRIVATE);;
+        double initialBalance=Double.parseDouble(preferences.getString("balance", "0").toString());
+        balance.setHint(preferences.getString("balance", "0").toString());
     }
 
     public void saveSettings(View view) {
@@ -37,7 +45,17 @@ public class Settings extends Activity {
 
         String category = dropDown.getItemAtPosition(dropDown.getSelectedItemPosition()).toString();
         edit.putString("currencyType", category);
+
+        try {
+            double balanceValue = Double.parseDouble(balance.getText().toString());
+            edit.putString("balance", balance.getText().toString());
+        }
+         catch (NumberFormatException e) {
+            Toast.makeText(Settings.this, "Balance must be a number!", Toast.LENGTH_LONG).show();
+        }
         edit.commit();
         db.updateCurrency(category);
+        Intent intent = new Intent(Settings.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/gretab/expensemanager/Statistics.java
+++ app/src/main/java/gretab/expensemanager/Statistics.java
@@ -1,6 +1,7 @@
 package gretab.expensemanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -14,13 +15,25 @@ import android.widget.TextView;
 public class Statistics extends Activity {
 
     DatabaseHelper db;
-    TextView sum;
+    TextView sum, balance;
+    SharedPreferences preferences;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
         db = new DatabaseHelper(this);
+        preferences = getSharedPreferences("Preferences", MODE_PRIVATE);
+        double initialBalance=Double.parseDouble(preferences.getString("balance", "0").toString());
+
         sum=(TextView)findViewById(R.id.textView11);
+        balance=(TextView)findViewById(R.id.balanceValue);
+
         sum.setText(Double.toString(db.getSpendings()));
+        balance.setText(Double.toString(initialBalance+db.getSpendings()));
+    }
+
+    public void openMainActivity(View view) {
+        Intent intent = new Intent(Statistics.this, MainActivity.class);
+        startActivity(intent);
     }
 }
